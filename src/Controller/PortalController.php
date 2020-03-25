0cecda708d8046f3ba7ef66b23de859a55ef0638
Etienne TRAN <etiennetran@hotmail.fr>
2020-03-25 22:23:58 +0100

    Création de la page d'accueil

---
+++ src/Controller/PortalController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PortalController extends AbstractController
+{
+    /**
+     * @Route("/portal", name="portal")
+     */
+    public function index()
+    {
+        return $this->render('portal/index.html.twig', [
+            'controller_name' => 'PortalController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+        return $this->render('portal/home.html.twig', [
+            'username' => 'Admin',
+        ]);
+    }
+}
