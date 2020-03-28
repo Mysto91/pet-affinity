c00c894bbd3c388c522325c49327c2b3087c1c11
Etienne TRAN <etiennetran@hotmail.fr>
2020-03-28 18:03:56 +0100

    Ajout d'un formulaire de création de feature

--- src/Controller/PortalController.php
+++ src/Controller/PortalController.php
@@ -4,15 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Feature;
 use App\Repository\FeatureRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PortalController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
-    public function home(){
+    public function home()
+    {
         return $this->render('portal/home.html.twig', [
             'username' => 'Admin',
             'url' => '/home'
@@ -39,6 +44,38 @@ class PortalController extends AbstractController
         return $this->render('portal/about.html.twig', [
             'controller_name' => 'PortalController',
         ]);
+    }
+
+    /**
+     * @Route("/feature/new", name="feature_create")
+     */
+    public function createFeature(Request $request, EntityManagerInterface $manager)
+    {
+        $feature = new Feature();
+
+        $form = $this->createFormBuilder($feature)
+            ->add('name')
+            ->add('description')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $feature->setCreatedAt(new \DateTime());
+            $manager->persist($feature);
+            $manager->flush();
+
+            return $this->redirectToRoute('feature', [
+                'id' => $feature->getId()
+            ]);
+        }
+
+        return $this->render(
+            'feature/create.html.twig',
+            [
+                'formFeature' => $form->createView(),
+            ]
+        );
     }
 
     /**
