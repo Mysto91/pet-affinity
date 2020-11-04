0e5642d3859964f80a7ff12e9b02bcfe88b20712
Etienne XPS <etiennetran@hotmal.fr>
2020-11-04 10:03:41 +0100

    Mise à jour symfony

--- src/DataFixtures/PetFixtures.php
+++ src/DataFixtures/PetFixtures.php
@@ -5,7 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Pet;
 use App\Entity\TypePet;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
 class PetFixtures extends Fixture
--- src/Repository/PetRepository.php
+++ src/Repository/PetRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 use App\Entity\Pet;
 use App\Entity\PetSearch;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Pet|null find($id, $lockMode = null, $lockVersion = null)
